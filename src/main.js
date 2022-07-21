940b8c7a0abea755b625b33a8b138a22847409cd
Herman Dardon <hdardon91@gmail.com>
2022-07-20 23:09:55 -0700

    fix content load

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import "@/assets/styles/base.css";
 const useImage = (url) => {
   // const newURL = new URL(`/src/${url}`, import.meta.url).href;
   // console.log(newURL);
-  return new URL(`/src/${url}`, import.meta.url).href;
+  return new URL(`/src${url}`, import.meta.url).href;
 };
 
 const app = createApp(App);
